219180042880fd3598aafadc14c588832f3dbd8b
olkuska <olkysha10@gmail.com>
2021-08-21 19:28:15 +0300

    init: finish task

--- src/index.js
+++ src/index.js
@@ -35,12 +35,37 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
-};
-
-function decode(expr) {
+  };
+  
+  function decode(expr) {
     // write your solution here
+    let str = expr.match (/.{1,10}/g);
+    let newStr = [];
+    let point = /10/g;
+    let dash = /11/g;
+    let zero = /00/g;
+    
+    for (let i = 0; i < str.length; i++) {
+      let symbol = str[i].split("**********").join("  ");
+    
+      symbol = symbol.replace(point, ".");
+      symbol = symbol.replace(dash, "-");
+      symbol = symbol.replace(zero, "");
+    
+      newStr.push(symbol);
+    }
+      newStr = newStr.join(" ");
+    
+      return newStr
+      .split("   ") // get word code 3 spaces apart
+             .map(word => word
+                           .split(" ") // get character code 1 spaces apart
+                           .map(character => MORSE_TABLE[character]) // decode Morse code character
+                           .join('')
+              )
+              .join(' ') // add spaces between words 
+              .trim()
 }
-
-module.exports = {
+  module.exports = {
     decode
-}
+  }
